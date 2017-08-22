ccc24db4e515b200303de59e7258a9f448d7c780
Tomwey <tomwey@163.com>
2017-08-22 11:47:17 +0800

    ccc

--- public/js/map-handle.js
+++ public/js/map-handle.js
@@ -1,116 +1,3 @@
-/*var map;
-    
-function initMap() {
-  map = new AMap.Map('map',{
-    resizeEnable: true,
-    zoom: 5,
-                    // center: [116.480983, 40.0958]
-  });
-            
-  var scale = new AMap.Scale({});
-  map.addControl(scale);
-};
-    
-initMap();
-
-// 添加小标记
-function addSmallMarkers(pois) {
-  var _markers = [];
-  if (!pois) return [];
-  
-  for (var i = 0; i < pois.length; i++) {
-    var marker = new AMap.Marker({
-      position: [pois[i].longitude, pois[i].latitude],//marker所在的位置
-      map: map, //创建时直接赋予map属性
-      offset: new AMap.Pixel(-13, -13), //相对于基点的偏移位置
-      content: '<div class="marker-container small-marker"><div class="text">'+ pois[i].platecount +'</div></div>',
-      extData: { level: 9 },
-    });
-    marker.on('click', function(e) {
-      var _marker = e.target;
-      var _map = _marker.getMap();
-      var newLevel = _marker.getExtData().level;
-      var level = _map.getZoom();
-      if (level < newLevel) {
-        _map.setZoomAndCenter(newLevel, _marker.getPosition());
-      }
-    });
-    _markers.push(marker);
-  }
-  // for (var i = 0; i<pois.length; i++) {
-  //   // console.log(pois[i]);
-  // }                                                                                             }
-  return _markers;
-}
-
-// 添加大标记
-function addLargeMarkers(pois) {
-  var markers = [];
-    for (var i = 0; i<pois.length; i++) {
-      var marker = new AMap.Marker({
-        position: [pois[i].lng, pois[i].lat],//marker所在的位置
-        map: map, //创建时直接赋予map属性
-        offset: new AMap.Pixel(-42, -42), //相对于基点的偏移位置
-        content: '<div class="marker-container large-marker"><div class="text">' +
-                    '<p class="title">'+ pois[i].cityname +'</p>' + 
-                    '<p>当前存量: '+ pois[i].curr_stock +'</p>' +
-                    '<p>去化周期: '+ pois[i].sell_length +'</p>' + 
-                    '<p>年销量: '+ pois[i].year_sale +'</p>' +
-                    '</div></div>',
-        extData: { level: 13 },
-      });
-                                         
-      marker.on('click', function(e) {
-        var _marker = e.target;
-        var _map = _marker.getMap();
-        var newLevel = _marker.getExtData().level;
-        var level = _map.getZoom();
-        if (level < newLevel) {
-           _map.setZoomAndCenter(newLevel, _marker.getPosition());
-        }});
-                                                     
-      markers.push(marker);
-    }      
-    return markers;
-}
-
-var markers;
-
-// 监听地图缩放事件
-AMap.event.addListener(map,'zoomend',function(){
-  console.log(map.getZoom());
-  map.remove(markers);
-  if ( map.getZoom() >= 13 ) {
-    markers = addLargeMarkers(points2);
-  } else if (map.getZoom() >= 9) {
-    // markers = addLargeMarkers(points);
-    loadCityMapData(function (res) {
-      console.log(res);
-      markers = addLargeMarkers(res.data);
-    }, function (error) {
-      console.log(error);
-    });
-    
-    // 显示指标面板
-    $("#stat-panel").animate({left: '10px'});
-  } else {
-    markers = addSmallMarkers(points);
-    // 隐藏指标面板
-    $("#stat-panel").animate({ left: '-300px'});
-  }
-});    
-
-$(document).ready(function() {
-  // console.log(1111);
-  
-  loadCityMapData(function (res) {
-    console.log(res);
-    markers = addSmallMarkers(res.data);
-  }, function (error) {
-    console.log(error);
-  });
-});*/
-
 window.CM_Map = {
   markers: [],
   map: null,
@@ -310,50 +197,76 @@ window.CM_Map = {
     for (var i=0; i<markerDataArr.length; i++) {
       var markerData = markerDataArr[i];
       // console.log(markerData);
-      var money = markerData.dealmoney > 100000000 ? (markerData.dealmoney / 100000000).toFixed(1).toString() + '亿' 
+      var money = markerData.dealmoney >= 100000000 ? (markerData.dealmoney / 100000000).toFixed(1).toString() + '亿' 
       : (markerData.dealmoney / 10000.00).toFixed(1).toString() + '万';
       
-      var saleNum = markerData.dealnum > 10000 ? (markerData.dealnum / 10000).toFixed(1).toString() + '万套' 
+      var saleNum = markerData.dealnum >= 10000 ? (markerData.dealnum / 10000).toFixed(1).toString() + '万套' 
       : markerData.dealnum.toString() + '套';
       
-      var area = markerData.dealarea > 100000 ? (markerData.dealarea / 100000).toFixed(1).toString() + '万㎡' 
+      var area = markerData.dealarea >= 100000 ? (markerData.dealarea / 100000).toFixed(1).toString() + '万㎡' 
       : markerData.dealarea.toString() + '㎡';
       
       // extData.cityName = markerData.cityname;
       
-      var tmpData = { level: extData.level, cityName: markerData.cityname };
+      // var tmpData = { level: extData.level, cityName: markerData.cityname };
       
-      var marker = new AMap.Marker({
-        position: [markerData.longitude, markerData.latitude],//marker所在的位置
-        map: this.map, //创建时直接赋予map属性
-        offset: new AMap.Pixel(-60, -60), //相对于基点的偏移位置
-        content: '<div class="marker-container large-marker"><div class="text">' +
-                    '<p class="title">'+ markerData.platename + '</p>' + 
-                    '<p>成交金额:'+ money +'</p>' +
-                    '<p>成交套数:'+ saleNum +'</p>' + 
-                    '<p>成交面积:'+ area +'</p>' +
-                    '</div></div>',
-        extData: tmpData,
-      });
-      marker.on('click', (e) => {
-        var _marker = e.target;
-        var _map = _marker.getMap();
-        var newLevel = _marker.getExtData().level;
-        var level = _map.getZoom();
-        
-        this.cityName = _marker.getExtData().cityName;
-        
-        if (level < newLevel) {
-          _map.setZoomAndCenter(newLevel, _marker.getPosition());
-        }
-        
-        // this.cityName = markerData.cityname;
-        $(document).trigger('marker:click', _marker.getExtData());
-        // $(document).trigger('marker:click', [this.cityName, newLevel]);
-        
-      });
+      var dataArr = [{
+          label: '成交金额',
+          value: money,
+        },{
+          label: '成交套数',
+          value: saleNum,
+        },{
+          label: '成交面积',
+          value: area,
+        },];
+      
+      var marker = this._createLargeMarkerForData(markerData.platename, dataArr, extData.level, markerData);
+      
       this.markers.push(marker);
     } // done add markers
+  },
+  _createLargeMarkerContentFromData: function(title, dataArr) {
+    // console.log(dataArr);
+    html = '<div class="marker-container large-marker"><div class="marker-content"><p class="title">'+ title +'</p><table class="table">';
+    for (var i = 0; i < dataArr.length; i++) {
+      var item = dataArr[i];
+      html += '<tr><td class="label"><p>' + item.label + ':</p></td><td class="value">'+ item.value +'</td></tr>';
+    }
+    html += '</table></div></div>';
+    return html;
+  },
+  _createLargeMarkerForData: function(title, dataArr, level, markerData) {
+    var tmpData = { level: level, cityName: markerData.cityname };
+    
+    var marker = new AMap.Marker({
+      position: [markerData.longitude, markerData.latitude],//marker所在的位置
+      map: this.map, //创建时直接赋予map属性
+      offset: new AMap.Pixel(-60, -60), //相对于基点的偏移位置
+      content: this._createLargeMarkerContentFromData(title, dataArr),
+      extData: tmpData,
+    });
+    
+    marker.on('click', (e) => {
+      var _marker = e.target;
+      var _map = _marker.getMap();
+      var newLevel = _marker.getExtData().level;
+      var level = _map.getZoom();
+      
+      this.cityName = _marker.getExtData().cityName;
+      
+      if (level < newLevel) {
+        _map.setZoomAndCenter(newLevel, _marker.getPosition());
+      }
+      
+      // this.cityName = markerData.cityname;
+      
+      $(document).trigger('marker:click', _marker.getExtData());
+      
+      // $(document).trigger('marker:click', [this.cityName, newLevel]);
+      
+    });
+    return marker;
   },
   addCityDetailMarkers: function(markerDataArr, extData = { level: 13 }) {
     if (!markerDataArr || markerDataArr.length === 0) return;
@@ -363,42 +276,33 @@ window.CM_Map = {
     this.markers = [];
     for (var i=0; i<markerDataArr.length; i++) {
       var markerData = markerDataArr[i];
-      console.log(markerData);
+      // console.log(markerData);
       // extData.cityName = markerData.cityname;
       
-      var tmpData = { level: extData.level, cityName: markerData.cityname };
+      var storeNum = parseFloat(markerData.storenum);
+      storeNum = storeNum >= 10000 ? (storeNum / 10000).toFixed(0).toString() + '万㎡' : 
+      storeNum.toFixed(0).toFixed(0).toString() + '㎡';
       
-      var marker = new AMap.Marker({
-        position: [markerData.longitude, markerData.latitude],//marker所在的位置
-        map: this.map, //创建时直接赋予map属性
-        offset: new AMap.Pixel(-42, -42), //相对于基点的偏移位置
-        content: '<div class="marker-container large-marker"><div class="text">' +
-                    '<p class="title">'+ markerData.cityname +'</p>' + 
-                    '<p>当前存量: '+ markerData.storenum +'</p>' +
-                    '<p>去化周期: '+ markerData.cycle +'</p>' + 
-                    '<p>&emsp;年销量: '+ markerData.dealsalecount +'</p>' +
-                    '</div></div>',
-        extData: tmpData,
-      });
-      marker.on('click', (e) => {
-        var _marker = e.target;
-        var _map = _marker.getMap();
-        var newLevel = _marker.getExtData().level;
-        var level = _map.getZoom();
-        
-        this.cityName = _marker.getExtData().cityName;
-        
-        if (level < newLevel) {
-          _map.setZoomAndCenter(newLevel, _marker.getPosition());
-        }
-        
-        // this.cityName = markerData.cityname;
-        
-        $(document).trigger('marker:click', _marker.getExtData());
-        
-        // $(document).trigger('marker:click', [this.cityName, newLevel]);
-        
-      });
+      var cycle = parseFloat(markerData.cycle) <= 0.0 ? 0 : parseFloat(markerData.cycle).toFixed(0).toString() + '%';
+      
+      var saleCount = parseInt(markerData.dealsalecount);
+      saleCount = saleCount >= 10000 ? (saleCount / 10000).toFixed(0).toString() + '万套' : 
+      saleCount.toString() + '套';
+      
+      var dataArr = [{
+          label: '当前存量',
+          value: storeNum,
+        },{
+          label: '去化周期',
+          value: cycle,
+        },{
+          label: '年销量',
+          value: saleCount,
+        },
+      ];
+      
+      var marker = this._createLargeMarkerForData(markerData.cityname, dataArr, extData.level, markerData);
+      
       this.markers.push(marker);
     } // done add markers
   },
