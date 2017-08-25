ca0aaa34779d9788c7aa8d6047be6685253130f1
Tomwey <tomwey@163.com>
2017-08-25 17:37:29 +0800

    ccc

--- public/js/common.js
+++ public/js/common.js
@@ -451,3 +451,27 @@ $(document).ready(function() {
   });
   
 }); // end ready
+
+// 限制只能输入数字  
+$.fn.onlyNum = function () {  
+    $(this).keypress(function (event) {  
+        var eventObj = event || e;  
+        var keyCode = eventObj.keyCode || eventObj.which;  
+        // valid the input value (according to the regex)  
+        return regNum(this.value + String.fromCharCode(keyCode)+0) ;  
+    }).focus(function () {  
+    //禁用输入法  
+        this.style.imeMode = 'disabled';  
+    });  
+};  
+/* 正则校验 */  
+function regNum(number) {  
+    if (/^[0-9]+(\.[0-9]+)?$/.test(number))  
+        return true;  
+    else  
+        return false;  
+} 
+
+// 限制输入数字
+$('#other-filter-start').onlyNum();
+$('#other-filter-end').onlyNum();
